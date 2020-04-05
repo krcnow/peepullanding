ad78c255b982e549326247612a3058eb0c888c2c
krcnow <37628523+krcnow@users.noreply.github.com>
2020-04-05 12:55:54 +0530

    added mockvideo

--- src/pages/simple.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import CustomQueryStringComponent from "../components/customQueryStringComponent"
-
-export default () => (
-  <div style={{ padding: 10 }}>
-    <CustomQueryStringComponent />
-  </div>
-)
